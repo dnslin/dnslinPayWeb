6281c275583c2fca4c18d16260e3d34494327f32
dnslin <1636988859@qq.com>
2021-11-25 19:34:17 +0800

    增加链接转二维码

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <pay></pay>
   </div>
 </template>
 
 <script>
+import Pay from './components/Pay.vue'
+import QRCode from 'qrcodejs2'
 export default {
+  components: { Pay, QRCode },
   name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Pay.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="qrcode" ref="qrCodeUrl"></div>
+</template>
+
+<script>
+import QRCode from 'qrcodejs2'
+export default {
+  data: function () {
+    return {
+      qrcode: '123'
+    }
+  },
+  methods: {
+    creatQrCode () {
+      let qrcode = new QRCode(this.$refs.qrCodeUrl, {
+        text: 'https://dnslin.com',
+        width: 100,
+        height: 100,
+        colorDark: '#000000',
+        colorLight: '#ffffff',
+        correctLevel: QRCode.CorrectLevel.H
+      })
+      this.qrcode = qrcode
+      console.log(this.qrcode)
+    }
+  },
+
+  mounted () {
+    this.creatQrCode()
+  }
+}
+</script>
+
+<style>
+
+</style>
